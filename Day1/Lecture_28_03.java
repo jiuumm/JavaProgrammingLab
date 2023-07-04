2c78616e4f86e7ddf0b2c3398ac6836d91cab789
Ji Young <jiyoung6301@gmail.com>
2023-07-04 10:06:18 +0900

    Create Lecture_28_03.java

---
+++ Day1/Lecture_28_03.java
@@ -0,0 +1,14 @@
+
+public class Lecture_28_03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double distance ;
+		double sec = 365*24*60*60;
+		distance = sec*3*10E4;
+		System.out.println(distance);
+		
+	}
+
+}
