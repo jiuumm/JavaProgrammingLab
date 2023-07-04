7478bc6b0bff2c9bbc95f3567edbb139bd00e64e
Ji Young <jiyoung6301@gmail.com>
2023-07-04 10:07:12 +0900

    Create Lecture_28_08.java

---
+++ Day1/Lecture_28_08.java
@@ -0,0 +1,19 @@
+
+public class Lecture_28_08 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x = 1, y=1;
+		int a = x++;
+		int b = ++y;
+		
+		System.out.println("a = "+a+ ", b = "+b );
+		
+		int c = 100, d=200;
+		c +=10;
+		d /=10;
+		System.out.println("c = "+c+ ", d = "+d);
+	}
+
+}
