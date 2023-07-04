fe4c27a8c538c7e278768d8819b961bf1e93c2ba
Ji Young <jiyoung6301@gmail.com>
2023-07-04 10:06:40 +0900

    Create Lecture_28_06.java

---
+++ Day1/Lecture_28_06.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Lecture_28_06 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("첫 번째 숫자를 입력하시오: ");
+		int i1 = sc.nextInt();
+		System.out.println("두 번째 숫자를 입력하시오: ");
+		int i2 = sc.nextInt();
+		System.out.println(i1+i2);
+	}
+
+}
