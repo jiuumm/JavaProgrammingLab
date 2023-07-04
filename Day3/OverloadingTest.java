f12bca7ca975e05ac8f020a0b288d06a29d14526
Ji Young <jiyoung6301@gmail.com>
2023-07-04 10:10:24 +0900

    Create OverloadingTest.java

---
+++ Day3/OverloadingTest.java
@@ -0,0 +1,14 @@
+
+public class OverloadingTest {
+//	public int getSum(int i, int j) {
+//		return i+j;
+//	}
+//	public double getSum(int i, int j) {
+//		return(double)i+j;
+//	}
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//
+//	}
+
+}
