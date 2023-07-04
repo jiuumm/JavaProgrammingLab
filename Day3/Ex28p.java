17f3fbb04bed7741879d8090223f4bf4e41f8c5d
Ji Young <jiyoung6301@gmail.com>
2023-07-04 10:09:53 +0900

    Create Ex28p.java

---
+++ Day3/Ex28p.java
@@ -0,0 +1,12 @@
+
+public class Ex28p {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String []s = {"Pepperoni", "Mushrooms", "Onions", "Sausage", "Bacon"};
+		for(String t : s) {
+			System.out.print(t+" ");
+		}
+	}
+
+}
