9d5c825561edfceb0eae97558905af9d79a9f717
Ji Young <jiyoung6301@gmail.com>
2023-07-04 10:06:22 +0900

    Create Lecture_28_04.java

---
+++ Day1/Lecture_28_04.java
@@ -0,0 +1,13 @@
+
+public class Lecture_28_04 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final double PI= 3.141592;
+		double radius =5.0;
+		double area = PI*radius*radius;
+		System.out.println(area);
+		
+	}
+
+}
