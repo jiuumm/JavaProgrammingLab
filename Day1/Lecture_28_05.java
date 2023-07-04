aef778ae8e98ae6ee839cbc5c30c8e2f6bcb2797
Ji Young <jiyoung6301@gmail.com>
2023-07-04 10:06:26 +0900

    Create Lecture_28_05.java

---
+++ Day1/Lecture_28_05.java
@@ -0,0 +1,18 @@
+
+public class Lecture_28_05 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		double f;
+		f= 1/5;
+		System.out.println(f);
+		
+		f=(double) 1/5;
+		System.out.println(f);
+		
+		i = (int )1.7+(int )1.8;
+		System.out.println(i);
+	}
+
+}
