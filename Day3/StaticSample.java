7c648bd5c07d4e48a318384f7570abc69605b862
Ji Young <jiyoung6301@gmail.com>
2023-07-04 10:10:58 +0900

    Create StaticSample.java

---
+++ Day3/StaticSample.java
@@ -0,0 +1,18 @@
+
+public class StaticSample {
+	 int n;
+	void g() {
+		
+	}
+	
+	static int m;
+	static void f() {
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StaticSample.m=10;
+		
+	}
+
+}
